a6be86e6eb9e9a2dbc6adce72f63a7f4eed92f3c
Blake Franklin <46824874+superdooper9000@users.noreply.github.com>
2019-04-14 13:02:40 -0500

    added trace messages

--- Final Project Submission/Franklin_EscapeRoomGame.cpp
+++ Final Project Submission/Franklin_EscapeRoomGame.cpp
@@ -20,65 +20,79 @@ void statusUpdate();
 
 int main()
 {
+  cout << "Here is a welcome message: " << endl;
 //welcome message
-//function
+  selectDifficulty();
 //  difficulty menu
 //  user selects difficulty
 //  function returns difficulty
-//function
+  rules();
 //  give user lit of commands and rules
-//function
+  roomDescription();
 //  give user description of Room
 //loop
-//  function
+  getCommand();
 //    get user input for command
 //    format input so it can be read
 //    return command
-//  function
+  resultOfCommand();
 //    check result of command
 //    update variables based on result
 //    display result in text
-//  function
+  statusUpdate();
 //    check health
 //    incriment and check time
 //    check game status
+  cout << "Here is the end-of-game message:" << endl;
 //display end-of-game message
 }
 
 int selectDifficulty()
 {
+  cout << "Here is the difficulty menu" << endl;
   //  difficulty menu
   //  user selects difficulty
+  cout << "You have selcted difficulty " << difficulty << endl;
   //  function returns difficulty
 }
 
 void rules()
 {
+  cout << "Here are the rules and commands:" << endl;
   //  give user lit of commands and rules
 }
 
 void roomDescription()
 {
+  cout << "Here is a description of the room:" << endl;
   //  give user description of Room
 }
 
 string getCommand()
 {
+  cout << "Getting user input for command" << endl;
   //    get user input for command
   //    format input so it can be read
+  cout << "Got command: " << command << endl;
   //    return command
 }
 
 void resultOfCommand()
 {
+  cout << "checking the result of the command..." << endl;
   //    check result of command
+  cout << "Updating variables.." << endl;
   //    update variables based on result
+  cout << "Here is the result:" << endl;
   //    display result in text
 }
 
 void statusUpdate()
 {
+  cout << "Checking player health..." << endl;
   //    check health
+  cout << "Checking time..." << endl;
   //    incriment and check time
+  cout << "Checking game status..." << endl;
   //    check game status
 }
