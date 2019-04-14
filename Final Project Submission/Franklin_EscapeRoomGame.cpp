7f9d8764197d8c03db183aca2219fcd94d77629a
Blake Franklin <46824874+superdooper9000@users.noreply.github.com>
2019-04-14 12:51:24 -0500

    added empty functions

--- Final Project Submission/Franklin_EscapeRoomGame.cpp
+++ Final Project Submission/Franklin_EscapeRoomGame.cpp
@@ -10,18 +10,24 @@
 
 using namespace std;
 
+int selectDifficulty();
+void rules();
+void roomDescription();
+string getCommand();
+void resultOfCommand();
+void statusUpdate();
 
 
 int main()
 {
+//welcome message
 //function
-//  welcome message
 //  difficulty menu
 //  user selects difficulty
 //  function returns difficulty
 //function
-//  game starts
 //  give user lit of commands and rules
+//function
 //  give user description of Room
 //loop
 //  function
@@ -37,6 +43,42 @@ int main()
 //    incriment and check time
 //    check game status
 //display end-of-game message
+}
 
+int selectDifficulty()
+{
+  //  difficulty menu
+  //  user selects difficulty
+  //  function returns difficulty
+}
 
+void rules()
+{
+  //  give user lit of commands and rules
+}
+
+void roomDescription()
+{
+  //  give user description of Room
+}
+
+string getCommand()
+{
+  //    get user input for command
+  //    format input so it can be read
+  //    return command
+}
+
+void resultOfCommand()
+{
+  //    check result of command
+  //    update variables based on result
+  //    display result in text
+}
+
+void statusUpdate()
+{
+  //    check health
+  //    incriment and check time
+  //    check game status
 }
