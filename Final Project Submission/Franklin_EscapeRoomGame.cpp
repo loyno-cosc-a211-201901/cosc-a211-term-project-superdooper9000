c3f345afd2603f8eaab3aae9616a1f42d4ef2206
Blake Franklin <46824874+superdooper9000@users.noreply.github.com>
2019-04-14 12:41:37 -0500

    pseudocode

---
+++ Final Project Submission/Franklin_EscapeRoomGame.cpp
@@ -0,0 +1,42 @@
+
+//Escape Room Game
+//-text-based adventure
+
+//This program will be a game that continually takes user inputs as commands and translates
+//them into things that will happen in the Game
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+int main()
+{
+//function
+//  welcome message
+//  difficulty menu
+//  user selects difficulty
+//  function returns difficulty
+//function
+//  game starts
+//  give user lit of commands and rules
+//  give user description of Room
+//loop
+//  function
+//    get user input for command
+//    format input so it can be read
+//    return command
+//  function
+//    check result of command
+//    update variables based on result
+//    display result in text
+//  function
+//    check health
+//    incriment and check time
+//    check game status
+//display end-of-game message
+
+
+}
